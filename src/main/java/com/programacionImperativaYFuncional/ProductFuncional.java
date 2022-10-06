9991869e283dda9b08140e82eb4a975c57e65426
milamaranth <mila.sanchezcarrero@gmail.com>
2022-10-06 12:29:22 +0200

    Todo con BigDecimal

--- src/main/java/com/programacionImperativaYFuncional/ProductFuncional.java
+++ src/main/java/com/programacionImperativaYFuncional/ProductFuncional.java
@@ -2,6 +2,8 @@ package com.programacionImperativaYFuncional;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ProductFuncional{
 
@@ -23,10 +25,34 @@ public class ProductFuncional{
             .map(x -> aplicarImpuest(x.price, x.tax.percent))
             .reduce(BigDecimal.ZERO, (x,y) -> x.add(y));
 
+        List<String> porC = shoppingCart.stream()
+            .filter(x -> x.name.startsWith("C"))
+            .sorted((x,y) -> x.name.compareToIgnoreCase(y.name))
+            .map(x -> x.name)
+            .collect(Collectors.toList());
+            //.collect(Collectors.joining(", ", "Empezando por C: ", "."));
+        
+        BigDecimal ten = new BigDecimal(10);
+        String mayor10empiezaPorC = shoppingCart.stream()
+            .filter(x -> x.price.compareTo(ten) > 0)
+            .sorted((x,y) -> x.name.compareToIgnoreCase(y.name))
+            .map(x -> x.name)
+            .filter(n -> porC.contains(n))
+            .collect(Collectors.joining(", ", "Con precio mayor que 10 y empezando por C: ", "."));
+
+        //List<Product> filtrado = shoppingCart.filter(p);
+
+        //s -> s.price.compare(10.0) > 0 && s.name.startsWith("C");
+
         System.out.println("El total es " + amount + ". Con impuestos: " + suma);
+        System.out.println(mayor10empiezaPorC);
     }
 
     public static BigDecimal aplicarImpuest(BigDecimal price, int taxPercent) {
         return price.multiply(BigDecimal.ONE.add(BigDecimal.valueOf(taxPercent).divide(BigDecimal.valueOf(100))));
     }
+
+
+
+    
 }
